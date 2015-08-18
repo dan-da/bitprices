76aa26732d2fabcd366147172d878942c8727914
danda <danda@osc.co.cr>
2015-08-18 08:55:42 -0700

    move insight -> insight_multiaddr and add new insight api class that uses insight's single addr API

--- lib/blockchain_api.php
+++ lib/blockchain_api.php
@@ -40,6 +40,7 @@ class blockchain_api_toshi  {
         
         // Todo:  make more robust with timeout, retries, etc.
         $buf = @file_get_contents( $url );
+file_put_contents( '/tmp/toshi.json', $buf );
         
         // note: http_response_header is set by file_get_contents.
         // next line will throw exception wth code 1001 if response code not found.
@@ -146,17 +147,9 @@ class blockchain_api_btcd implements blockchain_api {
         $url = sprintf( 'http://%s:%s@%s:%s/', $params['btcd-rpc-user'], $params['btcd-rpc-pass'],
                                           $params['btcd-rpc-host'], $params['btcd-rpc-port']);
 echo $url . "\n";
-/*
-        $rpc = Tivoka\Client::connect( $url );
-        $response = $rpc->sendRequest('getinfo', $params = null );
-        print_r( $response );
-        exit;
-        
-        print $request->result;// 42
-*/
         $rpc = new BitcoinClient( $url, false, 'BTC' );
         
-        $tx_list = $rpc->searchrawtransactions( $addr, $verbose=1, $skip=0, $count=1000000 );
+        $tx_list = $rpc->searchrawtransactions( $addr, $verbose=1, $skip=0, $count=1000000, $vinExtra=true );
 /*        
         print_r( $tx_list );
         exit;
@@ -237,8 +230,9 @@ echo $url . "\n";
     
 }
 
-
-class blockchain_api_insight  {
+// Bitpay Insight using multi address api.
+// note: experimental.  not recommended for use.
+class blockchain_api_insight_multiaddr  {
     
     public function get_addresses_transactions( $addr_list, $params ) {
         
@@ -252,7 +246,7 @@ class blockchain_api_insight  {
         
         // Todo:  make more robust with timeout, retries, etc.
         $buf = @file_get_contents( $url );
-file_put_contents( '/tmp/insight.json', $buf );        
+file_put_contents( '/tmp/insight_multiaddr.json', $buf );
         
         // note: http_response_header is set by file_get_contents.
         // next line will throw exception wth code 1001 if response code not found.
@@ -350,3 +344,118 @@ file_put_contents( '/tmp/insight.json', $buf );
     }
     
 }
+
+
+class blockchain_api_insight  {
+    
+    public function get_addresses_transactions( $addr_list, $params ) {
+        $tx_list = array();
+        foreach( $addr_list as $addr ) {
+            $tx_list_new = $this->get_address_transactions( $addr, $params );
+            $tx_list = array_merge( $tx_list, $tx_list_new );
+        }
+        return $tx_list;
+    }
+    
+    protected function get_address_transactions( $addr, $params ) {
+        
+        // note:  insight /api/txs does not presently seem to support a limit.
+        $addr_tx_limit = $params['addr-tx-limit'];
+        
+        $url_mask = "%s/api/txs?address=%s";
+        $url = sprintf( $url_mask, $params['insight'], $addr );
+        
+        mylogger()->log( "Retrieving transactions from $url", mylogger::info );
+        
+        // Todo:  make more robust with timeout, retries, etc.
+        $buf = @file_get_contents( $url );
+file_put_contents( '/tmp/insight.json', $buf );
+        
+        // note: http_response_header is set by file_get_contents.
+        // next line will throw exception wth code 1001 if response code not found.
+        $server_http_code = httputil::http_response_header_http_code( @$http_response_header );
+        
+        if( $server_http_code == 404 ) {
+            return array();
+        }
+        else if( $server_http_code != 200 ) {
+            throw new Exception( "Got unexpected response code $server_http_code" );
+        }
+        
+        $data = json_decode( $buf, true );
+        $tx_list = @$data['txs'];
+//print_r( $tx_list ); exit;        
+        
+        return $this->normalize_transactions( $tx_list, $addr );
+    }
+    
+    protected function normalize_transactions( $tx_list_insight, $addr ) {
+        
+//print_r( $tx_list_insight );        
+        $tx_list_normal = array();
+        foreach( $tx_list_insight as $tx_insight ) {
+            $amount_in = 0;
+            $amount_out = 0;
+            $amount = 0;
+            
+            $idx = 0;
+            foreach( $tx_insight['vin'] as $input ) {
+                $idx ++;
+                
+                // $input['addr'] can be empty, eg for a coinbase transaction
+                //    $input['coinbase'] will be set instead.
+                if( !@$input['addr'] ) {
+                    continue;
+                }
+                
+                // more than one address typically means multisig.  we ignore it.
+                // Example address with multi-sig 1AJbsFZ64EpEfS5UAjAfcUG8pH8Jn3rn1F
+                //     tx = 2daea775df11a98646c475c04247b998bbed053dc0c72db162dd6b0a99a59c26
+                if( @count( $input['addr'] ) > 1 ) {
+                    $msg = sprintf( "Unsupported number of addresses (%s) in input #%s, transaction %s.  skipping input.", @count( $input['addresses'] ), $idx, $tx_insight['hash'] );
+                    // mylogger()->log( $msg, mylogger::warning );
+                    continue;
+                }
+                
+                if( @$input['addr'] == $addr ) {
+                    $amount_out = $amount_out + btcutil::btc_to_int( $input['value'] );
+                }
+            }
+            $idx = 0;
+            $not_understood = array( 'multisig' );
+            foreach( $tx_insight['vout'] as $output ) {
+                $idx ++;
+                
+                // more than one address typically means multisig.  we ignore it.
+                // Example address with multi-sig 1AJbsFZ64EpEfS5UAjAfcUG8pH8Jn3rn1F
+                //     tx = 2daea775df11a98646c475c04247b998bbed053dc0c72db162dd6b0a99a59c26
+                if( @count( @$output['addresses'] ) > 1 ) {
+                    $msg = sprintf( "Unsupported number of addresses (%s) in output #%, transaction %s.  skipping output.", count( $output['addresses'] ), $idx, $tx_insight['hash'] );
+                    // mylogger()->log( $msg, mylogger::warning );
+                    continue;
+                }
+                // script_type can be hash160, p2sh or multisig.
+                // If we include multisig in balance calcs, then our balances do not match with bitcoind.
+                // So for now we include hash160 and p2sh only.
+                // See also: https://github.com/coinbase/insight/issues/189
+                if( @$output['scriptPubKey']['addresses'][0] == $addr && !in_array( $output['scriptPubKey']['type'], $not_understood ) ) {
+                    $amount_in = $amount_in + btcutil::btc_to_int( $output['value'] );
+                }
+            }
+            $amount = $amount_in - $amount_out;
+                        
+            $tx_list_normal[] = array( 'block_time' => $tx_insight['blocktime'],
+                                       'addr' => $addr,
+                                       'amount' => $amount,
+                                       'amount_in' => $amount_in,
+                                       'amount_out' => $amount_out,
+                                       'txid' => $tx_insight['txid'],
+                                     );
+            
+        }
+        
+        return $tx_list_normal;
+    }
+    
+}
+
